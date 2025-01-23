3abd1b4580384a6c8092e92b314df5daec0e2d39
jiwonclvl <clvlajrrhtlvek@gmail.com>
2025-01-23 09:37:45 +0900

    [level 1] Title: 내적, Time: 4.19 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -0,0 +1,15 @@
+import java.util.stream.IntStream;
+class Solution {
+    public int solution(int[] a, int[] b) {
+        int result = 0;
+        
+        //for문
+        // for (int i = 0; i < a.length; i++) {
+        //     result += a[i] * b[i];
+        // }
+        
+        //스트림
+        result = IntStream.range(0,a.length).map(i -> a[i] * b[i]).sum();
+        return result;
+    }
+}
