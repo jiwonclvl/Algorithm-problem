9e4202a9c2685981d6a321570610345f78e32155
jiwon <clvlajrrhtlvek@gmail.com>
2025-09-08 17:18:54 +0900

    [Bronze I] Title: 평균, Time: 236 ms, Memory: 21264 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -3,24 +3,29 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<Integer> grades = new ArrayList<>();
-        double result = 0;
-
+        
         int n = sc.nextInt();
-
-        for (int i = 0; i < n; i++) {
+        
+        double[] grades = new double[n];
+        int maxGrade = 0;
+        double sum = 0.0;
+        
+        for(int i = 0; i < n; i++) {
             int grade = sc.nextInt();
-            grades.add(grade);
+            
+            if (grade > maxGrade) {
+                maxGrade= grade;    
+            }
+             
+            grades[i] = (double) grade;
         }
-
-        int max = Collections.max(grades);
-
-        for (Integer grade : grades) {
-            double newGrade = (double) grade / max * 100;
-
-            result += newGrade;
+        
+        for (double grade : grades) {
+            double newGrade = grade / maxGrade * 100;
+            sum += newGrade;
         }
-
-        System.out.printf("%.6f", result/n);
+        
+        System.out.printf("%.2f", sum / n);
+        
     }
 }
